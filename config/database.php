ec1bcdf81da6b58b8bd63f51a295523f5cc61210
Djelloul Abid <digitalblueskye@gmail.com>
2026-07-15 22:56:50 +0200

    config database mysql

--- config/database.php
+++ config/database.php
@@ -5,15 +5,11 @@ $username = 'root';
 $password = '';
 
 try {
-    // Connexion à la base de données avec utf8mb4
-    $pdo = new PDO("mysql:host=$host;port=3307;dbname=$db_name;charset=utf8mb4;unix_socket=/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    // Fixer l'encodage pour toutes les interactions avec la base de données
-    $pdo->exec("SET NAMES 'utf8mb4'");  // Force l'encodage des requêtes SQL
-    $pdo->exec("SET CHARACTER SET utf8mb4");  // Définit l'encodage de la connexion
-    $pdo->exec("SET SESSION collation_connection = 'utf8mb4_general_ci'");  // Définit la collation de la connexion
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $pdo->exec("SET NAMES 'utf8mb4'");
 } catch (PDOException $e) {
-    die("Erreur : " . $e->getMessage());
+    die("Erreur de connexion : " . $e->getMessage());
 }
 ?>
